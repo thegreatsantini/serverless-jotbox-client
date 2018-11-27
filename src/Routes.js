44df93e2639c70bd9ec5395d3eb7e938a9c1b193
thegreatsantini <lss13@my.fsu.edu>
2018-11-26 17:39:19 -0800

    added childProps logiv

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from './containers/Login'
 import NotFound from './components/NotFound'
+import AppliedRoute from './components/AppliedRoute'
 
-export default () =>
+export default ({ childProps }) =>
     <Switch>
-        <Route path="/" exact component={Home} />
+        <AppliedRoute path="/" exact component={Home} props={childProps} />
+        <AppliedRoute path="/login" exact component={Login} props={childProps} />
+        {/* <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} /> */}
         <Route component={NotFound} />
     </Switch>;
