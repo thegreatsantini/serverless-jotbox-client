42805aba62aeb9d4e9ef1d04038d3d2ed601a58d
thegreatsantini <lss13@my.fsu.edu>
2018-11-26 17:39:02 -0800

    added child props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import NavBar from "./components/NavBar";
 import Routes from './Routes'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    console.log('set user')
+    this.setState({ isAuthenticated: authenticated });
+  }
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
     return (
       <React.Fragment>
-        <NavBar />
-        <Routes />
+        <NavBar childProps={childProps} />
+        <Routes childProps={childProps} />
       </React.Fragment>
     );
   }
