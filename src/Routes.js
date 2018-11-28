f7c3431b400fc6cfe7cc65e94a162b93d44e6eea
thegreatsantini <lss13@my.fsu.edu>
2018-11-27 22:35:58 -0800

    added single draft view route

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Login from './containers/Login';
 import Signup from './containers/Signup';
 import SandBox from './SandBox'
 import NewDraft from './containers/NewDraft'
+import Draft from './components/Draft'
 import NotFound from './components/NotFound';
 import AppliedRoute from './components/AppliedRoute';
 
@@ -14,6 +15,7 @@ export default ({ childProps }) =>
         <AppliedRoute path="/login" exact component={Login} props={childProps} />
         <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
         <AppliedRoute path="/newdraft" exact component={NewDraft} props={childProps} />
+        <AppliedRoute path="/draft/:id" exact component={Draft} props={childProps} />
         {/* <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} /> */}
         <Route path='/sandbox' component={SandBox} />
