146615c6bc6b3f5afbe5ba5915d18e8a03b59a85
thegreatsantini <lss13@my.fsu.edu>
2018-11-29 10:13:03 -0800

    edited new prompt component

--- src/components/NewPrompt.js
+++ src/components/NewPrompt.js
@@ -12,7 +12,6 @@ const styles = theme => ({
         marginBottom: '5px',
         // paddingTop: "50px",
         textAlign: "center",
-        border: '2px solid white'
     },
     button: {
         margin: theme.spacing.unit,
@@ -23,6 +22,12 @@ const styles = theme => ({
     promptContainer: {
         display: "flex",
         justifyContent: 'space-around'
+    },
+    paper: {
+        margin: theme.spacing.unit,
+    },
+    prompt: {
+        textAlign: 'center'
     }
 });
 
@@ -35,16 +40,22 @@ class NewPrompt extends React.Component {
         }
     }
 
-    renderPrompt = () => {
-        return this.state.prompt.map((item, i) => {
-            return (
-                <Fade in={true} key={i} timeout={item.fadeTime}>
-                    {/* <Typography component="h3"> */}
-                    <h3>{item.text}</h3>
-                    {/* </Typography> */}
+    renderPrompt = (classes) => {
+        const prompt = this.state.prompt.map(val => val.text).join(' ')
+        // console.log(prompt)
+        return (
+            <div className={classes.promptContainer}>
+                <Fade in={true} timeout={2000} >
+                   <Typography component='h3' className={classes.prompt}>
+                   <h3>{prompt}</h3>
+                       </Typography> 
                 </Fade>
-            )
-        })
+                <Button onClick={this.fetchPrompt} variant="contained" size='small' color="secondary" className={classes.button}>
+                    Next
+                </Button>
+               
+            </div>
+        )
     }
 
     fetchPrompt = async () => {
@@ -58,7 +69,7 @@ class NewPrompt extends React.Component {
                 promptArr.push({ text: prompt.components[1].words[1].text, fadeTime: Math.floor(Math.random() * 5000) + 500 })
                 promptArr.push({ text: prompt.components[2].english, fadeTime: Math.floor(Math.random() * 5000) + 500 })
                 promptArr.push({ text: prompt.components[3].english, fadeTime: Math.floor(Math.random() * 5000) + 500 })
-                this.props.setPrompt(promptArr)
+                // this.props.setPrompt(promptArr)
                 return promptArr
             })
             .then(res => this.setState({ prompt: res }))
@@ -80,12 +91,7 @@ class NewPrompt extends React.Component {
                 {
                     this.state.prompt.length < 1
                         ? this.renderButton(classes)
-                        : <div className={classes.promptContainer}>
-                            {this.renderPrompt()}
-                            <Button onClick={this.fetchPrompt} variant="contained" size='small' color="secondary" className={classes.button}>
-                                Next
-                            </Button>
-                        </div>
+                        : this.renderPrompt(classes)
                 }
             </Paper>
         )
