c43a1435dbcc5da6a0b2c471cc3069d3a88fb53e
thegreatsantini <lss13@my.fsu.edu>
2018-11-30 13:07:55 -0800

    created Genres List comp for profile

---
+++ src/components/DraftCards.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Icon from '@material-ui/core/Icon';
+import { Link } from 'react-router-dom';
+
+const styles = theme => ({
+    icon: {
+        margin: theme.spacing.unit * 2,
+        color: 'black'
+    },
+});
+
+const shortenPrompt = (prompt) => prompt.substring(0, 30) + "..."
+
+const DraftCards = (props, styles) => {
+    const { classes } = props
+    return props.drafts.map((item, i) => {
+        return (
+            <React.Fragment key={i}>
+                <ListItem >
+                    <Icon
+                        component={Link}
+                        to={`/draft/${item.draftId}`}
+                        className={classes.icon}
+                    >
+                        edit
+                    </Icon>
+                    <ListItemText primary={item.title} secondary={shortenPrompt(item.prompt)} />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+            </React.Fragment>
+        )
+    })
+}
+
+
+export default withStyles(styles)(DraftCards);
