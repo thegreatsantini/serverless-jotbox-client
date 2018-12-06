91153ca68fd1bc33e78d28dc87d15d0bb429197b
thegreatsantini <lss13@my.fsu.edu>
2018-12-05 19:44:42 -0800

    users login with username instead of email

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -18,7 +18,7 @@ const styles = theme => ({
         paddingBottom: theme.spacing.unit * 4,
         margin: "90px 80px",
         // margin: '0 auto',
-        
+
     },
     container: {
         display: 'flex',
@@ -61,14 +61,14 @@ class Login extends React.Component {
         super()
         this.state = {
             isLoading: false,
-            email: '',
+            userName: '',
             password: '',
         };
 
     }
 
     validateForm() {
-        return this.state.email.length > 0 && this.state.password.length > 0;
+        return this.state.userName.length > 0 && this.state.password.length > 0;
     }
 
     handleChange = name => event => {
@@ -81,7 +81,10 @@ class Login extends React.Component {
         event.preventDefault();
         this.setState({ isLoading: true });
         try {
-            await Auth.signIn(this.state.email, this.state.password);
+            await Auth.signIn(this.state.userName, this.state.password)
+                .then(user => console.log(user))
+                .catch(err => console.log(err));
+
             this.props.userHasAuthenticated(true);
             this.props.history.push("/");
         } catch (e) {
@@ -105,11 +108,11 @@ class Login extends React.Component {
                 <form className={classes.container} noValidate autoComplete="off">
                     <TextField
                         id="filled-email-input"
-                        label="Email"
+                        label="User Name"
                         className={classes.textField}
-                        type="email"
+                        type="text"
                         autoComplete="email"
-                        onChange={this.handleChange('email')}
+                        onChange={this.handleChange('userName')}
                         margin="normal"
                     />
                     <TextField
