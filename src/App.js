deeb2efbca486ea0438b05dae0d85940a97c4791
thegreatsantini <lss13@my.fsu.edu>
2018-12-10 20:11:22 -0800

    got appjs working old style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Auth } from "aws-amplify";
 import NavBar from "./components/NavBar";
-import Routes from './Routes'
+import Routes from './Routes';
+import { API } from 'aws-amplify';
 
 class App extends Component {
   constructor(props) {
@@ -9,38 +10,42 @@ class App extends Component {
 
     this.state = {
       isAuthenticated: false,
-      isAuthenticating: true,
-      userName: null
+      isAuthenticating: true
     };
   }
+
+  // getUserName = () =>{
+  //   return API.get('users', '/user')
+
+  // }
 
   async componentDidMount() {
     try {
       await Auth.currentSession();
-      // console.log('current Session', test)
-      await Auth.currentAuthenticatedUser()
-      .then(user => {
-        this.setState({userName: user.username})
-        // Auth.userAttributes(user)
-        this.userHasAuthenticated(true);
-      })
-      // .then(attributes => console.log('hello', attributes))
-        .catch(err => console.log('APP.JS current user', err));
+      // Auth.currentAuthenticatedUser()
+      //   .then(user => Auth.userAttributes(user))
+      //   .then(attributes => console.log(attributes))
+      //   .catch(err => console.log(err));
+      // console.log(test)
+      this.userHasAuthenticated(true);
     }
     catch (e) {
       if (e !== 'No current user') {
         alert(e);
       }
     }
+
     this.setState({ isAuthenticating: false });
   }
 
-  userHasAuthenticated = authenticated => {
+  userHasAuthenticated = async authenticated => {
+
     this.setState({ isAuthenticated: authenticated });
   }
+
+
   render() {
     const childProps = {
-      userName: this.state.userName,
       isAuthenticated: this.state.isAuthenticated,
       userHasAuthenticated: this.userHasAuthenticated
     };
